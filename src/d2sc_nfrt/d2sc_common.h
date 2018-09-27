fdde9bab578d4512115360f8819e1bd225a79f06
god14fei <god14fei@hust.edu.cn>
2018-09-27 13:32:39 +0800

    NF runtime

---
+++ src/d2sc_nfrt/d2sc_common.h
@@ -0,0 +1,12 @@
+/*********************************************************************
+ *                     D2SC
+ *              https://github.com/god14fei/d2sc
+ *
+ *   BSD LICENSE
+ *
+ *   Copyright(c)
+ *            2018-2019 Huazhong University of Science and Technology
+ *
+ *   All rights reserved.
+ *
+ ********************************************************************/
