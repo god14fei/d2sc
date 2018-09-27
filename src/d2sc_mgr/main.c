1cc2da6a4b85485ad5f30d513b49246392e401e4
god14fei <god14fei@hust.edu.cn>
2018-09-27 13:26:48 +0800

    source code folder

---
+++ src/d2sc_mgr/main.c
@@ -0,0 +1,19 @@
+/*********************************************************************
+ *                     D2SC
+ *              https://github.com/god14fei/d2sc
+ *
+ *   BSD LICENSE
+ *
+ *   Copyright(c)
+ *            2018-2019 Huazhong University of Science and Technology
+ *
+ *   All rights reserved.
+ *
+ ********************************************************************/
+ 
+ /******************************************************************************
+                                   main.c
+     File containing the main function of the manager and all its worker
+     threads.
+ *
+ *****************************************************************************/
