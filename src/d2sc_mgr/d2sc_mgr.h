f6e0f5afd5ff0b0dbdad601c76cfcaab870c8799
god14fei <god14fei@hust.edu.cn>
2018-10-15 16:56:17 +0800

    header file for the manager

---
+++ src/d2sc_mgr/d2sc_mgr.h
@@ -0,0 +1,61 @@
+/*********************************************************************
+ *                            D2SC
+ *              https://github.com/god14fei/d2sc
+ *
+ *   BSD LICENSE
+ *
+ *   Copyright(c)
+ *            2018-2019 Huazhong University of Science and Technology
+ *
+ *   All rights reserved.
+ *
+********************************************************************/
+
+/******************************************************************************
+                                   d2sc_mgr.h
+      Header file containing all shared headers and data structures
+
+******************************************************************************/
+
+#ifndef _D2SC_MGR_H_
+#define _D2SC_MGR_H_
+
+
+/******************************Standard C library*****************************/
+
+#include <netinet/ip.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+/********************************DPDK library*********************************/
+
+#include <rte_byteorder.h>
+#include <rte_memcpy.h>
+#include <rte_malloc.h>
+#include <rte_fbk_hash.h>
+
+
+/******************************Internal headers*******************************/
+
+#include "d2sc_mgr/d2sc_args.h"
+#include "d2sc_mgr/d2sc_init.h"
+#include "d2sc_includes.h"
+#include "d2sc_sc.h"
+#include "d2sc_ft.h"
+#include "d2sc_fd.h"
+#include "d2sc_pkt_common.h"
+
+
+/***********************************Macros************************************/
+
+#define TO_PORT 0
+#define TO_NF 1
+
+
+/***************************Shared global variables***************************/
+
+/* ID to be assigned to the next NF instance that starts */
+extern uint16_t next_inst_id;
+
+#endif // _D2SC_MGR_H_
